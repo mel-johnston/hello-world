7b1a671b99d54446ad687ba3f68c023305826c78
MELANIE JOHNSTON <MELANIE.JOHNSTON.1@OUTLOOK.COM>
2022-11-17 21:44:40 +0000

    labs 7 and 8 test

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 
-let usersName = prompt('What is your name?');
-document.write('Hello ' + usersName + ', ');
+function sayHi() {
+    let usersName = prompt('What is your name?');
+    // document.write('Hello ' + usersName + '!');
+    if (usersName == "") {
+        alert("You didn't type a name...");
+        usersName = prompt("Please tell me your name.");
+    }
+    console.log("The user's name is: " + usersName);
+    document.write("Hello and welcome to my page, " + usersName);
 
-if (confirm("Do you like to travel?")) {
-    document.write(" since you like to travel, avoid these places!");
-} else {
-    document.write(" you've come to the right place to justify staying home.");
+    return usersName;
+
 }
+
+
+// if (confirm("Do you like to travel?")) {
+//         document.write(" since you like to travel, avoid these places!");
+//     } else {
+//         document.write(" you've come to the right place to justify staying home.");
+//     }
+
+function likesTravel() {
+    let response = prompt("Do you like to travel?")
+    if (response == "yes") {
+        alert("Welcome and be sure to avoid these places!");
+    } else if (response == "no") {
+        alert("You've come to the right place to justify staying home!");
+    } else {
+        alert("I'm not sure what that means...");
+    }
+}
+
 
 // function yourName() {
 //     let name = prompt('What is your name?');
@@ -32,7 +56,34 @@ if (confirm("Do you like to travel?")) {
 // }
 
 
+// function functionName(){
+//     code to execute;
+// }
+
+// function addTwoNumbers(number1, number2){
+//     let sum = number1 + number2;
+//     return sum;
+// }
+
+// console.log(addTwoNumbers(4,5));
+
+
+// How would you rate traveling, 1-10?
+
+// function numPlanes() {
+//     let response = prompt("How much do you like traveling, 1-10?")
+//     if (response == "1") { }
 
 
 
+// }
 
+function numPlanes() {
+    let output = '';
+    let rating = prompt('scale of 1-10, how many planes?');
+    for(let i = 0; i < rating; i++){
+        output += "<img class='plane' src='plane.png'/>";
+    }
+
+    return document.write(output);
+}
